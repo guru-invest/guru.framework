e50bb04be4a47ef13f3a2fca4cc1d92fe36daffa
Tiago Sanches <tisanches@gmail.com>
2019-11-14 14:19:14 -0300

    adiciona alguns codigos de status

---
+++ helpers/statuscode.go
@@ -0,0 +1,18 @@
+package helpers
+
+const (
+	Status_Ok					 = 200
+	Status_Created				 = 201
+	Status_Accepted				 = 202
+	Status_NoContent			 = 204
+
+	Status_BadRequest			 = 400
+	Status_Unauthorized			 = 401
+	Status_Forbidden			 = 403
+	Status_NotFound				 = 404
+	Status_RequestTimeout		 = 408
+
+	Status_InternalServerError	 = 500
+	Status_BadGateway			 = 502
+	Status_ServiceUnavailable	 = 503
+)
