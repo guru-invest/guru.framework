1af18990ec294d1806b343bfbf104ad8fcf1b829
Tiago Sanches <tisanches@gmail.com>
2019-11-18 18:12:35 -0300

    adiciona testes

--- tests/security/auth/token_test.go
+++ tests/security/auth/token_test.go
@@ -1,9 +1,7 @@
-package security
+package auth_test
 
 import (
-	b64 "encoding/base64"
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/dgrijalva/jwt-go"
@@ -11,16 +9,33 @@ import (
 	"github.com/guru-invest/guru.framework/src/security/claims"
 )
 
-func TestTokenGeneration(t *testing.T) {
+func createValidToken() auth.TokenStructure {
 	DocumentNumber := "5kCTnuVt"
 	Email := "tom@guru.com.vc"
 	Fullname := "Tom teste de geração de token"
-	authBackend := auth.InitJWTAuthenticationBackend("")
+	authBackend := auth.InitJWTAuthenticationBackend(DocumentNumber)
 	m := make(map[string]string)
 	m["id"] = DocumentNumber
 	m["email"] = Email
 	m["fullname"] = Fullname
-	token := authBackend.GenerateToken(m, 260000)
+	return authBackend.GenerateToken(m, 260000)
+}
+
+func createInvalidToken() auth.TokenStructure {
+	DocumentNumber := "5kCTnuVt"
+	Email := "tom@guru.com.vc"
+	Fullname := "Tom teste de geração de token"
+	authBackend := auth.InitJWTAuthenticationBackend("InvalidToken")
+	m := make(map[string]string)
+	m["id"] = DocumentNumber
+	m["email"] = Email
+	m["fullname"] = Fullname
+	return authBackend.GenerateToken(m, 260000)
+}
+
+func TestTokenGeneration(t *testing.T) {
+
+	token := createValidToken()
 	if token.Token == "" {
 		fmt.Print("Null token")
 		t.Fail()
@@ -30,15 +45,14 @@ func TestTokenGeneration(t *testing.T) {
 }
 
 func TestTokenValidation(t *testing.T) {
-	tokenString := "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IiIsImRvY3VtZW50IjoibURrcEQzM2ciLCJmdWxsbmFtZSI6Ikd1cnUgYXBwIiwibG9jYWxlIjoiVTJVZ2RtOWp3Nm9nWTI5dWMyVm5kV2wxSUdOb1pXZGhjaUJoZE1PcElHRnhkV2tzSUhCdlpHVWdkR1Z5SUdObGNuUmxlbUVnY1hWbElHVnpkR0Z0YjNNZ2FXNTBaWEpsYzNOaFpHOXpJR1Z0SUhOaFltVnlJSEYxWlcwZ2RtOWp3Nm9ndzZrdUlFMWhibVJsSUhWdElHVnRZV2xzSUhCaGNtRWdkRzl0UUdkMWNuVXVZMjl0TG5aaklHVWdkbUZ0YjNNZ1ltRjBaWElnZFcwZ2NHRndieTRnUVdKeVljT25iM01oIiwiZXhwIjoxNTY3NzA2MDU5fQ.DWhcVVJ5eQ6Sj4EwDLBUhrGQ8Hn-5mTQD3596jHHQQVVVpepYojSRYOFMq1b8LMrFkLNl77IRcFdQFLby_2q7w"
-	tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
+
+	tokenString := createValidToken().Token
 	if tokenString != "" {
 		token, err := jwt.ParseWithClaims(tokenString, &claims.Claims{}, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 			}
-			sEnc := b64.StdEncoding.EncodeToString([]byte(token.Claims.(*claims.Claims).DocumentNumber))
-			return []byte(sEnc), nil
+			return []byte(token.Claims.(*claims.Claims).DocumentNumber), nil
 		})
 
 		if _, ok := token.Claims.(*claims.Claims); ok && token.Valid {
