a04f1895ff67d1214ebf2e613394bc9317e03678
Tiago Sanches <tisanches@gmail.com>
2019-12-12 17:12:22 -0300

    altera response

--- src/router/returns/response.go
+++ src/router/returns/response.go
@@ -1,19 +1,92 @@
 package returns
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// ErrorResponse is Error response template
+type ErrorResponse struct {
+	Message string `json:"reason"`
+	Error   error  `json:"-"`
+}
+
 type Response struct {
 	IsFailure bool
-	Messages  []string
+	Message   string
 	Result    interface{}
 }
 
-func (r Response) ToResponse(messages []string, result interface{}) Response {
-	isFailure := false
-	if messages != nil {
-		isFailure = true
+// printDebugf behaves like log.Printf only in the debug env
+func printDebugf(format string, args ...interface{}) {
+	if env := os.Getenv("GO_SERVER_DEBUG"); len(env) != 0 {
+		log.Printf("[DEBUG] "+format+"\n", args...)
 	}
+}
 
-	r.IsFailure = isFailure
-	r.Messages = messages
-	r.Result = result
-	return r
+func (r Response) ToJson() []byte {
+	ret, _ := json.Marshal(r)
+	return ret
+}
+
+func (e *ErrorResponse) string() string {
+	return fmt.Sprintf("reason: %s, error: %s", e.Message, e.Error.Error())
+}
+
+// Respond is response write to ResponseWriter
+func respond(w http.ResponseWriter, code int, src interface{}) {
+	var body []byte
+	var err error
+
+	switch s := src.(type) {
+	case []byte:
+		if !json.Valid(s) {
+			Error(w, http.StatusInternalServerError, err, "invalid json")
+			return
+		}
+		body = s
+	case string:
+		body = []byte(s)
+	case *ErrorResponse, ErrorResponse:
+		// avoid infinite loop
+		if body, err = json.Marshal(src); err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("{\"reason\":\"failed to parse json\"}"))
+			return
+		}
+	default:
+		if body, err = json.Marshal(src); err != nil {
+			Error(w, http.StatusInternalServerError, err, "failed to parse json")
+			return
+		}
+	}
+	w.WriteHeader(code)
+	w.Write(body)
+}
+
+// Error is wrapped Respond when error response
+func Error(w http.ResponseWriter, code int, err error, msg string) {
+	e := &Response{
+		IsFailure: true,
+		Message:   msg,
+		Result:    err,
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	respond(w, code, e)
+}
+
+// JSON is wrapped Respond when success response
+//src is message success or struct/json/interface result
+func Ok(w http.ResponseWriter, code int, src interface{}) {
+	r := &Response{
+		IsFailure: false,
+		Message:   "",
+		Result:    src,
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	respond(w, code, r)
 }
