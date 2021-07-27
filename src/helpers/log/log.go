455c96b115d6debae8dfe9c2bf8f98d50d150054
Tiago Sanches <tisanches@gmail.com>
2021-07-27 14:13:14 -0300

    Develop (#22)
    * Adiciona lig log (#20)
    
    * Adiciona módulos novos
    
    * Adiciona configuration
    
    * Corrige bug de importação no configuration
    
    * Corrige bug de importação
    
    * Altera go.mod para utilizar as ultimas versões dos pacotes
    
    * adiciona retorno de erro no metodo post
    
    * remove dependencia do feeder md
    
    * adiciona audience e issuer no token
    
    * adiciona request id no header do response
    
    * add request id no header
    
    * cria v1
    
    * add request id no header
    
    * add request id no header
    
    * corrige cagadas
    
    * Adciona conexão ao Influx para log
    
    * Recebe dados de conexão
    
    * Adiciona service
    
    * remove requestID
    
    * adiciona customer code e ip no influx
    
    * remove async
    
    * altera metodo POST, sempre vai retornar o body
    
    * adiciona http delete
    
    * adiciona http delete
    
    * adiciona event Type no influx
    
    * adiciona novo registro de log
    
    Co-authored-by: Tom Bernardes <tom@guru.com.vc>
    Co-authored-by: Leandro Albuquerque <leandroalp@gmail.com>
    
    * resolve merge
    
    Co-authored-by: Tom Bernardes <tom@guru.com.vc>
    Co-authored-by: Leandro Albuquerque <leandroalp@gmail.com>

---
+++ src/helpers/log/log.go
@@ -0,0 +1,94 @@
+package log
+
+import (
+	"net/http"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+var sugar *zap.SugaredLogger
+
+type GuruLog struct {
+	HTTPHeader http.Header
+	/*
+		ClientIP      string
+		ServiceName   string
+		DeviceID      string
+		CorrelationID string
+		SessionID     string
+		UserAgent     map[string]interface{}
+	*/
+}
+type Fields map[string]interface{}
+
+type LogWithFields struct {
+	CustomerCode string
+	Message      Fields
+}
+
+func InitLog() {
+	logger, _ := zap.Config{
+		Encoding:         "json",
+		Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey: "message",
+
+			LevelKey:    "level",
+			EncodeLevel: zapcore.CapitalLevelEncoder,
+
+			TimeKey:    "time",
+			EncodeTime: zapcore.ISO8601TimeEncoder,
+
+			CallerKey:    "caller",
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}.Build()
+	sugar = logger.Sugar()
+}
+
+func (t GuruLog) createMessage(fields LogWithFields, message string) (string, []interface{}) {
+	return message, []interface{}{
+		"client-ip", t.HTTPHeader.Get("client-ip"),
+		"service-name", t.HTTPHeader.Get("service-name"),
+		"device-id", t.HTTPHeader.Get("device-id"),
+		"correlation-id", t.HTTPHeader.Get("correlation-id"),
+		"session-id", t.HTTPHeader.Get("session-id"),
+		"user-agent", t.HTTPHeader.Get("user-agent"),
+		"customer-code", fields.CustomerCode,
+		"message", fields.Message,
+	}
+}
+
+func (t GuruLog) Info(pFields LogWithFields, pMessage string) {
+	message, fields := t.createMessage(pFields, pMessage)
+	sugar.Infow(message, fields...)
+}
+
+func (t GuruLog) Error(pFields LogWithFields, pMessage string) {
+	message, fields := t.createMessage(pFields, pMessage)
+	sugar.Errorw(message, fields...)
+}
+
+func (t GuruLog) Debug(pFields LogWithFields, pMessage string) {
+	message, fields := t.createMessage(pFields, pMessage)
+	sugar.With(fields...).Debugw(message)
+}
+
+func (t GuruLog) Fatal(pFields LogWithFields, pMessage string) {
+	message, fields := t.createMessage(pFields, pMessage)
+	sugar.Fatalw(message, fields...)
+}
+
+func (t GuruLog) Panic(pFields LogWithFields, pMessage string) {
+	message, fields := t.createMessage(pFields, pMessage)
+	sugar.Panicw(message, fields...)
+}
+
+func (t GuruLog) Warning(pFields LogWithFields, pMessage string) {
+	message, fields := t.createMessage(pFields, pMessage)
+	sugar.With(fields...).Warnw(message)
+}
---
+++ tests/log/log_test.go
@@ -0,0 +1,34 @@
+package log_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/guru-invest/guru.framework/src/helpers/log"
+)
+
+func TestLogInfo(t *testing.T) {
+	log.InitLog()
+
+	guruLog := log.GuruLog{
+		HTTPHeader: map[string][]string{},
+	}
+	guruLog.HTTPHeader.Add("user-agent", "Version | teste2 |")
+	guruLog.HTTPHeader.Add("device-id", "ASD4AS56D4-4ASD54AS6D-4AS5D4AS65")
+	guruLog.HTTPHeader.Add("client-ip", "192.168.0.1")
+	guruLog.HTTPHeader.Add("service-name", "guru.framework")
+	guruLog.HTTPHeader.Add("correlation-id", "AAAAA1-AAAAA2-AAAAA3")
+	guruLog.HTTPHeader.Add("session-id", "FD45D84F7E")
+
+	guruLog.Error(log.LogWithFields{
+		CustomerCode: "customerCode",
+		Message: log.Fields{
+			"msg 1": "mensagem 1",
+			"msg 2": "mensagem 2",
+			"msg 3": "mensagem 3",
+		},
+	}, "primeiro log",
+	)
+
+	fmt.Println("OK")
+}
