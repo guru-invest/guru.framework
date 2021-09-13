e00cfc1b3d28b16aea7dee6be730a21637654b0a
Tiago Sanches <tisanches@gmail.com>
2021-09-13 10:53:03 -0300

    adiciona metodo create

--- src/events/clevertap.go
+++ src/events/clevertap.go
@@ -66,6 +66,21 @@ func (et *EventTracker) SendProfile(customer_code string, name string, gender st
 	return nil
 }
 
+func (et *EventTracker) CreateProfile(customer_code string, name string, email string, nickname string) error {
+	err := et.builder.Build().SendProfile(
+		customer_code,
+		map[string]interface{}{
+			"Name":     name,
+			"Email":    email,
+			"Nickname": nickname,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (et *EventTracker) SendName(customer_code string, name string) error {
 	builder := et.builder.Build()
 
