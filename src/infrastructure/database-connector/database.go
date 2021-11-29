866ae0af6fa49322a1fecf51b06088db26fa2573
Tiago Sanches <tisanches@gmail.com>
2021-11-29 19:03:54 -0300

    volta versao do gorm

--- src/infrastructure/database-connector/database.go
+++ src/infrastructure/database-connector/database.go
@@ -3,9 +3,9 @@ package database_connector
 import (
 	"fmt"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/pkg/errors"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 type DataBase struct {
@@ -16,7 +16,7 @@ type DataBase struct {
 func (c *DataBase) ConnectToDatabase(url string, username string, password string, dbname string, host string, port string) (*DataBase, error) {
 	dsn := fmt.Sprintf(url, username,
 		password, dbname, host, port)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open("postgres", dsn)
 	database := DataBase{
 		URI:        dsn,
 		Connection: db,
@@ -25,4 +25,12 @@ func (c *DataBase) ConnectToDatabase(url string, username string, password strin
 		return nil, errors.Wrap(err, "error on connecting to database")
 	}
 	return &database, nil
+}
+
+func (c *DataBase) DisconnectFromDatabase(database *DataBase) error {
+	err := database.Connection.Close()
+	if err != nil {
+		return errors.Wrap(err, "error on disconnecting from database")
+	}
+	return nil
 }
