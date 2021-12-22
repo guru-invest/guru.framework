f7a2204a4fc2647e69a4ad94943146437aceccd4
charoleizer <victor.t.y.z@hotmail.com>
2021-12-22 02:02:28 -0300

    adicionado cobertura para demais tipos de log
    não foi possivel cobrir o log fata, pois ele aborta a aplicação.

--- tests/log/log_test.go
+++ tests/log/log_test.go
@@ -8,12 +8,11 @@ import (
 	"github.com/guru-invest/guru.framework/src/helpers/log"
 )
 
-func TestLogInfo(t *testing.T) {
-	log.InitLog("INFO", "unit teste")
-
+func getGuruLog() log.GuruLog {
 	guruLog := log.GuruLog{
 		HTTPHeader: &http.Header{},
 	}
+
 	guruLog.HTTPHeader.Add("user-agent", "Version | teste2 |")
 	guruLog.HTTPHeader.Add("device-id", "ASD4AS56D4-4ASD54AS6D-4AS5D4AS65")
 	guruLog.HTTPHeader.Add("X-Forwarded-For", "192.168.0.1")
@@ -21,7 +20,28 @@ func TestLogInfo(t *testing.T) {
 	guruLog.HTTPHeader.Add("correlation-id", "AAAAA1-AAAAA2-AAAAA3")
 	guruLog.HTTPHeader.Add("session-id", "FD45D84F7E")
 
-	guruLog.Error(&log.LogWithFields{
+	return guruLog
+}
+
+func TestLogDefaultInfoFieldsNull(t *testing.T) {
+
+	log.InitLog("DEFAULT", "unit teste")
+
+	guruEmptyLog := log.GuruLog{
+		HTTPHeader: nil,
+	}
+
+	guruEmptyLog.Info(nil, "primeiro log")
+
+	fmt.Println("OK")
+}
+
+func TestLogInfo(t *testing.T) {
+	log.InitLog("INFO", "unit teste")
+
+	guruLog := getGuruLog()
+
+	guruLog.Info(&log.LogWithFields{
 		CustomerCode: "customerCode",
 		Caller:       "TestLogInfo",
 		InfoMessage: log.Fields{
@@ -35,14 +55,86 @@ func TestLogInfo(t *testing.T) {
 	fmt.Println("OK")
 }
 
-func TestLogInfoFieldsNull(t *testing.T) {
-	log.InitLog("INFO", "unit teste")
+func TestLogDebug(t *testing.T) {
+	log.InitLog("DEBUG", "unit teste")
 
-	guruLog := log.GuruLog{
-		HTTPHeader: nil,
-	}
+	guruLog := getGuruLog()
 
-	guruLog.Info(nil, "primeiro log")
+	guruLog.Debug(&log.LogWithFields{
+		CustomerCode: "customerCode",
+		Caller:       "TestLogDebug",
+		InfoMessage: log.Fields{
+			"msg 1": "mensagem 1",
+			"msg 2": "mensagem 2",
+			"msg 3": "mensagem 3",
+		},
+	}, "primeiro log",
+	)
+
+	fmt.Println("OK")
+}
+
+func TestLogError(t *testing.T) {
+	log.InitLog("ERROR", "unit teste")
+
+	guruLog := getGuruLog()
+
+	guruLog.Error(&log.LogWithFields{
+		CustomerCode: "customerCode",
+		Caller:       "TestLogError",
+		InfoMessage: log.Fields{
+			"msg 1": "mensagem 1",
+			"msg 2": "mensagem 2",
+			"msg 3": "mensagem 3",
+		},
+	}, "primeiro log",
+	)
+
+	fmt.Println("OK")
+}
+
+func TestLogWarn(t *testing.T) {
+
+	log.InitLog("WARN", "unit teste")
+
+	guruLog := getGuruLog()
+
+	guruLog.Warning(&log.LogWithFields{
+		CustomerCode: "customerCode",
+		Caller:       "TestLogWarn",
+		InfoMessage: log.Fields{
+			"msg 1": "mensagem 1",
+			"msg 2": "mensagem 2",
+			"msg 3": "mensagem 3",
+		},
+	}, "primeiro log",
+	)
+
+	fmt.Println("OK")
+}
+
+func TestLogPanic(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	log.InitLog("PANIC", "unit teste")
+
+	guruLog := getGuruLog()
+
+	guruLog.Panic(&log.LogWithFields{
+		CustomerCode: "customerCode",
+		Caller:       "TestLogPanic",
+		InfoMessage: log.Fields{
+			"msg 1": "mensagem 1",
+			"msg 2": "mensagem 2",
+			"msg 3": "mensagem 3",
+		},
+	}, "primeiro log",
+	)
 
 	fmt.Println("OK")
 }
