4e3eaea79214a0bf92ad64f93d91d7f100533c43
Tiago Sanches <tisanches@gmail.com>
2022-07-08 15:23:49 -0300

    adiciona conectar para postgres

--- src/infrastructure/database-connector/database.go
+++ src/infrastructure/database-connector/database.go
@@ -9,36 +9,32 @@ import (
 )
 
 type DatabaseConnector struct {
-	DataSource struct {
-		Port     int
-		URL      string
-		Username string
-		Password string
-		Database string
-	}
-	ConfigPool struct {
-		SetConnMaxLifetime time.Duration
-	}
+	Port               int
+	URL                string
+	Username           string
+	Password           string
+	Database           string
+	SetConnMaxLifetime time.Duration
 }
 
-func (t DatabaseConnector) ConnectForServless(databaseOption DatabaseConnector) (*sql.DB, error) {
+func (t DatabaseConnector) ConnectForServless() (*sql.DB, error) {
 
 	dsn := fmt.Sprintf(
 		"user=%s password=%s dbname=%s host=%s port=%d timezone=%s",
-		databaseOption.DataSource.Username,
-		databaseOption.DataSource.Password,
-		databaseOption.DataSource.Database,
-		databaseOption.DataSource.URL,
-		databaseOption.DataSource.Port,
+		t.Username,
+		t.Password,
+		t.Database,
+		t.URL,
+		t.Port,
 		"America/Sao_Paulo")
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, errors.Wrap(err, "error on connecting to database")
 	}
-	db.SetMaxOpenConns(1)                                               // maximo de nova conexao por pool de conexao
-	db.SetMaxIdleConns(0)                                               //maximo de conexão inativa aguardando reuso
-	db.SetConnMaxLifetime(databaseOption.ConfigPool.SetConnMaxLifetime) //tempo maximo para expirar uma conexao
+	db.SetMaxOpenConns(1)                       // maximo de nova conexao por pool de conexao
+	db.SetMaxIdleConns(0)                       //maximo de conexão inativa aguardando reuso
+	db.SetConnMaxLifetime(t.SetConnMaxLifetime) //tempo maximo para expirar uma conexao
 
 	return db, nil
 }
