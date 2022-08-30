ef6ef1ac16f7e351ce8188be07dabb09f9e4d3c7
Tiago Sanches <tisanches@gmail.com>
2022-08-30 14:29:05 -0300

    Access token genial (#30)
    * adiciona middleware para authorizacao
    
    * adiciona middleware para consultar token genial direto no redis
    
    * adiciona campos map external ids no autenticarted user

--- src/models/AuthenticatedUser.go
+++ src/models/AuthenticatedUser.go
@@ -8,15 +8,17 @@ type AuthenticatedUser struct {
 	JwtToken string `json:"-"`
 	DeviceID string `json:"device_id"`
 	Customer struct {
-		CustomerCode   string `json:"customer_code"`
-		Name           string `json:"name"`
-		NickName       string `json:"nick_name"`
-		Email          string `json:"email"`
-		DocumentNumber string `json:"document_number"`
+		CustomerCode        string `json:"customer_code"`
+		Name                string `json:"name"`
+		NickName            string `json:"nick_name"`
+		Email               string `json:"email"`
+		DocumentNumber      string `json:"document_number"`
+		HasPassword         string `json:"has_password"`
+		UserAcceptMigration *bool  `json:"user_accept_migration"`
 	} `json:"customer"`
 	External struct {
 		CustomerID         string            `json:"external_id"`
-		PartnerIds         map[string]string `json:"partner_ids"`
+		PartnerIDs         map[string]string `json:"partner_ids"`
 		Status             string            `json:"status"`
 		SuitabilityProfile string            `json:"suitability"`
 	} `json:"external"`
