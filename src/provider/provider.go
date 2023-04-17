6b3b38e32d034fc62d646fd96a14c06e29384265
Leonardo Costa <31902416+lbcosta@users.noreply.github.com>
2023-04-17 10:32:45 -0300

    Adicionar Injeção de Dependência ao Framework (wrapper around uber-go/fx) (#33)
    * branch init
    
    * fixing variadic constructors
    
    * fixing variadic constructors on NewProductionApp
    
    * fixing Api interface problem with fx.Provide
    
    * separating router constructor from other constructors
    
    * trying fx.supply
    
    * using provide+annotate+as
    
    * removing router constructor call
    
    * changing params pattern of newapp

---
+++ src/provider/provider.go
@@ -0,0 +1,68 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"github.com/apex/gateway"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+	"strconv"
+)
+
+var appPort string
+
+type AppOptions struct {
+	ApiPort           string
+	RouterConstructor interface{}
+	AppConstructors   []interface{}
+}
+
+type Api interface {
+	Run()
+	Routes() *gin.Engine
+}
+
+func NewProductionApp(options AppOptions) *fx.App {
+	appPort = options.ApiPort
+	return fx.New(
+		fx.Provide(fx.Annotate(options.RouterConstructor, fx.As(new(Api)))),
+		fx.Provide(options.AppConstructors...),
+		fx.Invoke(func(lifecycle fx.Lifecycle, api Api) {
+			lifecycle.Append(
+				fx.Hook{
+					OnStart: func(context.Context) error {
+						port, _ := strconv.Atoi(appPort)
+						go gateway.ListenAndServe(fmt.Sprintf(":%d", port), api.Routes())
+						return nil
+					},
+				},
+			)
+		}),
+		// 	TODO: LOG ON CLOUD WATCH
+		//fx.WithLogger(func(logger *zap.Logger) fxevent.Logger {
+		//	return &fxevent.ZapLogger{Logger: logger}
+		//}),
+	)
+}
+
+func NewDevelopmentApp(options AppOptions) *fx.App {
+	appPort = options.ApiPort
+	return fx.New(
+		fx.Provide(fx.Annotate(options.RouterConstructor, fx.As(new(Api)))),
+		fx.Provide(options.AppConstructors...),
+		fx.Invoke(func(lifecycle fx.Lifecycle, api Api) {
+			lifecycle.Append(
+				fx.Hook{
+					OnStart: func(context.Context) error {
+						go api.Run()
+						return nil
+					},
+				},
+			)
+		}),
+		// TODO: LOG ON CONSOLE AND CLOUDWATCH
+		//fx.WithLogger(func(logger *zap.Logger) fxevent.Logger {
+		//	return &fxevent.ZapLogger{Logger: logger}
+		//}),
+	)
+}
