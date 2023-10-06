bef4e0835e7bfdb0afc7485048e8190a52bf86e0
Pedro Custódio <pedro.c.shd@gmail.com>
2023-10-06 14:34:05 -0300

    feature: return https staus code

--- src/infrastructure/http-connector/http.go
+++ src/infrastructure/http-connector/http.go
@@ -18,47 +18,47 @@ type HttpClient struct {
 	Timeout time.Duration
 }
 
-func (c *HttpClient) Get(uri string) ([]byte, error) {
+func (c *HttpClient) Get(uri string) (int, []byte, error) {
 	client := &http.Client{}
 	if c.Timeout != 0 {
 		client.Timeout = c.Timeout
 	}
-	
+
 	req, _ := http.NewRequest(http.MethodGet, uri, nil)
 	req.Header = c.Header
 	res, err := client.Do(req)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on executing get request")
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing get request")
 	}
 
 	reqBody, _ := ioutil.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
-		return reqBody, nil
+		return res.StatusCode, reqBody, nil
 	}
 
-	return reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
+	return res.StatusCode, reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
 }
 
-func (c *HttpClient) Post(uri string, v interface{}) ([]byte, error) {
+func (c *HttpClient) Post(uri string, v interface{}) (int, []byte, error) {
 
 	requestBody, err := json.Marshal(v)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on parsing request body")
+		return 500, []byte{}, errors.Wrap(err, "Error on parsing request body")
 	}
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodPost, uri, bytes.NewBuffer(requestBody))
 	req.Header = c.Header
 	res, err := client.Do(req)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on executing request")
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
 	}
 
 	reqBody, _ := ioutil.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
-		return reqBody, nil
+		return res.StatusCode, reqBody, nil
 	}
 
-	return reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
+	return res.StatusCode, reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
 }
 
 func formatRequest(r *http.Request) string {
@@ -87,11 +87,11 @@ func formatRequest(r *http.Request) string {
 	return strings.Join(request, "\n")
 }
 
-func (c *HttpClient) Delete(uri string, v interface{}) ([]byte, error) {
+func (c *HttpClient) Delete(uri string, v interface{}) (int, []byte, error) {
 
 	requestBody, err := json.Marshal(v)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on parsing request body")
+		return 500, []byte{}, errors.Wrap(err, "Error on parsing request body")
 	}
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodDelete, uri, bytes.NewBuffer(requestBody))
@@ -99,22 +99,22 @@ func (c *HttpClient) Delete(uri string, v interface{}) ([]byte, error) {
 
 	res, err := client.Do(req)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on executing request")
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
 	}
 
 	reqBody, _ := ioutil.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
-		return reqBody, nil
+		return res.StatusCode, reqBody, nil
 	}
 
-	return reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
+	return res.StatusCode, reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
 }
 
-func (c *HttpClient) Put(uri string, v interface{}) ([]byte, error) {
+func (c *HttpClient) Put(uri string, v interface{}) (int, []byte, error) {
 
 	requestBody, err := json.Marshal(v)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on parsing request body")
+		return 500, []byte{}, errors.Wrap(err, "Error on parsing request body")
 	}
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodPut, uri, bytes.NewBuffer(requestBody))
@@ -122,24 +122,24 @@ func (c *HttpClient) Put(uri string, v interface{}) ([]byte, error) {
 
 	res, err := client.Do(req)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on executing request")
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
 	}
 
 	reqBody, _ := ioutil.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
-		return reqBody, nil
+		return res.StatusCode, reqBody, nil
 	}
 
-	return reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
+	return res.StatusCode, reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
 }
 
-func (c *HttpClient) Patch(uri string, v interface{}) ([]byte, error) {
+func (c *HttpClient) Patch(uri string, v interface{}) (int, []byte, error) {
 
 	requestBodyBuffered := &bytes.Buffer{}
 	if v != nil {
 		requestBody, err := json.Marshal(v)
 		if err != nil {
-			return []byte{}, errors.Wrap(err, "Error on parsing request body")
+			return 500, []byte{}, errors.Wrap(err, "Error on parsing request body")
 		}
 
 		requestBodyBuffered = bytes.NewBuffer(requestBody)
@@ -148,20 +148,20 @@ func (c *HttpClient) Patch(uri string, v interface{}) ([]byte, error) {
 	req, err := http.NewRequest(http.MethodPatch, uri, requestBodyBuffered)
 	req.Header = c.Header
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on executing new request")
+		return 500, []byte{}, errors.Wrap(err, "Error on executing new request")
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		return []byte{}, errors.Wrap(err, "Error on executing request")
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
 	}
 
 	defer res.Body.Close()
 
 	reqBody, _ := ioutil.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
-		return reqBody, nil
+		return res.StatusCode, reqBody, nil
 	}
 
-	return reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
+	return res.StatusCode, reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
 }
