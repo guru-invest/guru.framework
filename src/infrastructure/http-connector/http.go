d747ded089d99904de7d5f291d5ff5ed858526bd
tisanches <tisanches@gmail.com>
2023-10-23 16:07:55 -0300

    refactory no retorno de erro do http connector, pois quando dava erro no request o response vinha como null e dava nil pointer

--- src/infrastructure/http-connector/http.go
+++ src/infrastructure/http-connector/http.go
@@ -3,11 +3,10 @@ package http_connector
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -28,10 +27,16 @@ func (c *HttpClient) Get(uri string) (int, []byte, error) {
 	req.Header = c.Header
 	res, err := client.Do(req)
 	if err != nil {
+		if os.IsTimeout(err) {
+			return http.StatusRequestTimeout, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+		if res == nil {
+			return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
 		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing get request")
 	}
 
-	reqBody, _ := ioutil.ReadAll(res.Body)
+	reqBody, _ := io.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		return res.StatusCode, reqBody, nil
 	}
@@ -50,10 +55,16 @@ func (c *HttpClient) Post(uri string, v interface{}) (int, []byte, error) {
 	req.Header = c.Header
 	res, err := client.Do(req)
 	if err != nil {
-		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
+		if os.IsTimeout(err) {
+			return http.StatusRequestTimeout, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+		if res == nil {
+			return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing get request")
 	}
 
-	reqBody, _ := ioutil.ReadAll(res.Body)
+	reqBody, _ := io.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		return res.StatusCode, reqBody, nil
 	}
@@ -61,37 +72,11 @@ func (c *HttpClient) Post(uri string, v interface{}) (int, []byte, error) {
 	return res.StatusCode, reqBody, errors.Wrap(errors.New(strconv.Itoa(res.StatusCode)), res.Status)
 }
 
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string
-	// Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	// Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-	// Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	}
-	// Return the request as a string
-	return strings.Join(request, "\n")
-}
-
 func (c *HttpClient) Delete(uri string, v interface{}) (int, []byte, error) {
 
 	requestBody, err := json.Marshal(v)
 	if err != nil {
-		return 500, []byte{}, errors.Wrap(err, "Error on parsing request body")
+		return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on parsing request body")
 	}
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodDelete, uri, bytes.NewBuffer(requestBody))
@@ -99,10 +84,16 @@ func (c *HttpClient) Delete(uri string, v interface{}) (int, []byte, error) {
 
 	res, err := client.Do(req)
 	if err != nil {
-		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
+		if os.IsTimeout(err) {
+			return http.StatusRequestTimeout, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+		if res == nil {
+			return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing get request")
 	}
 
-	reqBody, _ := ioutil.ReadAll(res.Body)
+	reqBody, _ := io.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		return res.StatusCode, reqBody, nil
 	}
@@ -114,18 +105,24 @@ func (c *HttpClient) Put(uri string, v interface{}) (int, []byte, error) {
 
 	requestBody, err := json.Marshal(v)
 	if err != nil {
-		return 500, []byte{}, errors.Wrap(err, "Error on parsing request body")
+		return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on parsing request body")
 	}
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodPut, uri, bytes.NewBuffer(requestBody))
 	req.Header = c.Header
-
 	res, err := client.Do(req)
 	if err != nil {
-		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
+		if os.IsTimeout(err) {
+			return http.StatusRequestTimeout, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+
+		if res == nil {
+			return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing get request")
 	}
 
-	reqBody, _ := ioutil.ReadAll(res.Body)
+	reqBody, _ := io.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		return res.StatusCode, reqBody, nil
 	}
@@ -139,26 +136,30 @@ func (c *HttpClient) Patch(uri string, v interface{}) (int, []byte, error) {
 	if v != nil {
 		requestBody, err := json.Marshal(v)
 		if err != nil {
-			return 500, []byte{}, errors.Wrap(err, "Error on parsing request body")
+			return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on parsing request body")
 		}
 
 		requestBodyBuffered = bytes.NewBuffer(requestBody)
 	}
 	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodPatch, uri, requestBodyBuffered)
+	req, _ := http.NewRequest(http.MethodPatch, uri, requestBodyBuffered)
 	req.Header = c.Header
-	if err != nil {
-		return 500, []byte{}, errors.Wrap(err, "Error on executing new request")
-	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing request")
+		if os.IsTimeout(err) {
+			return http.StatusRequestTimeout, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+
+		if res == nil {
+			return http.StatusInternalServerError, []byte{}, errors.Wrap(err, "Error on executing get request")
+		}
+		return res.StatusCode, []byte{}, errors.Wrap(err, "Error on executing get request")
 	}
 
 	defer res.Body.Close()
 
-	reqBody, _ := ioutil.ReadAll(res.Body)
+	reqBody, _ := io.ReadAll(res.Body)
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		return res.StatusCode, reqBody, nil
 	}
