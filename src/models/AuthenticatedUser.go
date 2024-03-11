093a62416351b3ee2682e321973df11361f8998f
tisanches <tisanches@gmail.com>
2024-03-11 11:46:11 -0300

    alter authenticated user model, alter property bonus to ma[string]string

--- src/models/AuthenticatedUser.go
+++ src/models/AuthenticatedUser.go
@@ -8,20 +8,18 @@ type AuthenticatedUser struct {
 	JwtToken string `json:"-"`
 	DeviceID string `json:"device_id"`
 	Customer struct {
-		CustomerCode         string `json:"customer_code"`
-		Name                 string `json:"name"`
-		NickName             string `json:"nick_name"`
-		Email                string `json:"email"`
-		DocumentNumber       string `json:"document_number"`
-		HasPassword          string `json:"has_password"`
-		CreatedAt            string `json:"created_at"`
-		HasActiveRLP         bool   `json:"has_active_rlp"`
-		UserAcceptCryptoTerm bool   `json:"user_accept_crypto_term"`
-		Bonus                struct {
-			ContaIncentivada string `json:"conta_incentivada"`
-		} `json:"bonus"`
-		HasAcceptTerms             bool   `json:"has_accept_terms"`
-		ProductInvestmentPeference string `json:"product_invest_preference"`
+		CustomerCode               string            `json:"customer_code"`
+		Name                       string            `json:"name"`
+		NickName                   string            `json:"nick_name"`
+		Email                      string            `json:"email"`
+		DocumentNumber             string            `json:"document_number"`
+		HasPassword                string            `json:"has_password"`
+		CreatedAt                  string            `json:"created_at"`
+		HasActiveRLP               bool              `json:"has_active_rlp"`
+		UserAcceptCryptoTerm       bool              `json:"user_accept_crypto_term"`
+		Bonus                      map[string]string `json:"bonus"`
+		HasAcceptTerms             bool              `json:"has_accept_terms"`
+		ProductInvestmentPeference string            `json:"product_invest_preference"`
 	} `json:"customer"`
 	External struct {
 		CustomerID         string            `json:"external_id"`
